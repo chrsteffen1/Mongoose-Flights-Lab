dd5f8bdcc815ce918f7d3974b556e93cf430cb58
Chrsteffen <Chrsteffen1@gmail.com>
2022-09-26 19:15:31 -0400

    set datetime-local

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,8 +4,11 @@ import {Flight} from '../models/flights.js'
 
 function newFlight(req,res) {
   const newFlight = new Flight()
+  const dt = newFlight.departs
+  const departsDate = dt.toISOString().slice(0, 16)
   res.render('flights/new', {
-    title: 'Add Flight'
+    title: 'Add Flight',
+    departsDate,
   })
 }
 
