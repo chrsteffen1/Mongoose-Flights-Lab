97457f483a9a705198ce6b73bad866a3a6f43b5a
Chrsteffen <Chrsteffen1@gmail.com>
2022-09-27 19:59:05 -0400

    render new meals page

---
+++ controllers/meals.js
@@ -0,0 +1,16 @@
+import { Meal } from '../models/meals.js'
+
+
+function newMeal(req,res){
+  Meal.find({})
+  .then(meals => {
+    res.render('meals/new', {
+      title: 'Add Meal',
+      meals,
+    })
+  })
+}
+
+export {
+  newMeal as new,
+}
